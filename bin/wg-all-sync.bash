5ed4ac24d789701c31cda72f5523a8bb19fe2c3a
Nill Rinov <nill@ingolonde.pw>
2022-04-11 22:37:27 +0200

    Create wg-all-sync.bash and skel/wg-hosts

---
+++ bin/wg-all-sync.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+filename='/etc/wireguard/wg-hosts'
+
+if test ! -f "$filename"; then
+    echo $filename does not exist && exit 255
+fi
+
+n=1
+while read line; do
+rsync /etc/wireguard/wg-all.conf $line:/etc/wireguard/
+n=$((n+1))
+done < $filename
